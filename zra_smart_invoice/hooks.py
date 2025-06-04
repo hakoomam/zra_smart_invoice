dc370480bb8a4b07c0e0418275a6e2befca32976
mhakooma <mh@antares.co.zm>
2025-06-04 15:41:27 +0200

    delete

--- zra_smart_invoice/hooks.py
+++
@@ -1,7 +0,0 @@
-
-doc_events = {
-    "Sales Invoice": {
-        "on_submit": "zra_smart_invoice.sales_invoice.on_submit_sales_invoice",
-        "validate": "zra_smart_invoice.sales_invoice.add_manual_retry_button"
-    }
-}
